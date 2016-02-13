4c152e6bf8a7afe47edaad8fcbeb53dbf3eadaf5
YuliyaYakimovaDev <yakimovayuliya@i.ua>
2016-02-13 19:55:16 +0200

    Finish user edit, update, index, and destroy actions

--- db/migrate/20160203201229_add_password_digest_to_users.rb
+++ db/migrate/20160203201229_add_password_digest_to_users.rb
@@ -1,5 +1,5 @@
-class AddRememberDigestToUsers < ActiveRecord::Migration
+class AddPasswordDigestToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :remember_digest, :string
+    add_column :users, :password_digest, :string
   end
 end
